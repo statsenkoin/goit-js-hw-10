a3f46ad07da4eebff887a286290118e7c47cb029
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-28 12:55:44 +0200

    create base fetching from restcountries.com

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://restcountries.com/v2';
+const endPoint = 'name';
+const searchFields = 'fields=name,capital,population,flags,languages';
+
+export function fetchCountries(country) {
+  return fetch(`${BASE_URL}/${endPoint}/${country}?${searchFields}`).then(
+    responce => {
+      if (!responce.ok) throw new Error(responce.statusText);
+      return responce.json();
+    }
+  );
+}
