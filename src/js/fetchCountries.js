505d7bc101ba065390096dbd876e45db322842d5
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-28 20:35:12 +0200

    change end point

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://restcountries.com/v2';
-const endPoint = 'name';
+const service = 'name';
 const searchFields = 'fields=name,capital,population,flags,languages';
 
 export function fetchCountries(country) {
-  return fetch(`${BASE_URL}/${endPoint}/${country}?${searchFields}`).then(
+  return fetch(`${BASE_URL}/${service}/${country}?${searchFields}`).then(
     responce => {
       if (!responce.ok) throw new Error(responce.status);
       return responce.json();
